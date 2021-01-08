5d8d7d2df7aebfbb583c0b1ed42a7805f2c09693
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-08 13:07:48 -0500

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+airlines = Airline.create([
+    {
+        name: "United Airlines",
+        image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+    },
+    {
+        name: "Southwest",
+        image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+    },
+    {
+        name: "Delta",
+        image_url: "https://open-flights.s3.amazonaws.com/Delta.png"
+    },
+    {
+        name: "Alaska Airlines",
+        image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png"
+    },
+    {
+        name: "JetBlue",
+        image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png"
+    },
+    {
+        name: "American Airlines",
+        image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png"
+    }
+])
+
+reviews = Review.create([
+    {
+        title: 'great airline',
+        description: "what a lovely time",
+        score: 5,
+        airline: airlines.first
+
+    },
+    {
+        title: 'bad airline',
+        description: "no help at all but madness",
+        score: 1,
+        airline: airlines.first
+
+    }
+
+])
