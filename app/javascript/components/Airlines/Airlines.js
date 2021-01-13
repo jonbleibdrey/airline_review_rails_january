f32c5e9a9d718723feffbd83f0db9a5ec6b6ae6e
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-13 15:43:47 -0500

    added hook

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Airlines = () => {
+    const[airlines, setAirlines] = useState([])
+
+        useEffect( () => {
+            
+        })
+
     return (
         <div>
             this is the Arlines index view from app
