cea3d1f68bc7b492bbc339422c6499ccae21890b
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-14 11:47:30 -0500

    added hooks and added divs

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+import Header from './Header'
 
-const Airlines = () => {
-  return <div>this is the Arlines show view page from app</div>;
+
+const Airlines = (props) => {
+
+    const [Airline,setAirline] = useState({})
+    const [review,setReview] = useState({})
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        const slug = props.match.params.slug
+        const url = `/api/v1/airlines/${slug}`
+
+        axios.get(url)
+        .then(resp => {setAirline(resp.data)
+        setLoaded(true)
+        })
+        .catch(resp => console.log(resp))
+    },[])
+
+  return (
+  <div className="wrapper">
+      <div className="column">
+          <Header attributes={airline.data.attributes}/>
+          <div className="reviews"></div>
+      </div>
+      <div className="column">
+        <div className="review-form">[review goes heere.]</div>
+      </div>
+
+  </div>
+  )
 };
+
 
 export default Airlines;
